f78921281f6b5e73720c4bb137c92063807822de
Matej Knopp <matej.knopp@gmail.com>
2023-07-27 19:36:39 +0200

    chore: add update_cargokit script

---
+++ update_cargokit.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Pub doesn't support sub modules (no recursive checkout) so we have to use git subtree instead.
+# This script will update the super_native_extensions subtree
+git subtree pull --prefix cargokit https://github.com/irondash/cargokit.git main --squash
